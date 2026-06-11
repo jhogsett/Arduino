b80baaa0b3206dcdd37f68e215b172704e19e68a
Jerry Hogsett <jhogsett@gmail.com>
2026-06-10 21:50:39 -0700

    adjust for triple ad9833 device

--- ad9833/call_sequence/call_sequence.ino
+++ ad9833/call_sequence/call_sequence.ino
@@ -24,15 +24,15 @@ static RandomSeed<RANDOM_SEED_PIN> randomizer;
 // Pins for SPI comm with the AD9833 IC
 const uint8_t PIN_DATA = 11;  ///< SPI Data pin number
 const uint8_t PIN_CLK = 13;  	///< SPI Clock pin number
-const uint8_t PIN_FSYNC1 = 10; ///< SPI Load pin number (FSYNC in AD9833 usage)
-const uint8_t PIN_FSYNC2 = 9;  ///< SPI Load pin number (FSYNC in AD9833 usage)
-const uint8_t PIN_FSYNC3 = 8;  ///< SPI Load pin number (FSYNC in AD9833 usage)
-const uint8_t PIN_FSYNC4 = 7;  ///< SPI Load pin number (FSYNC in AD9833 usage)
+const uint8_t PIN_FSYNC1 = 8; ///< SPI Load pin number (FSYNC in AD9833 usage)
+const uint8_t PIN_FSYNC2 = 7;  ///< SPI Load pin number (FSYNC in AD9833 usage)
+const uint8_t PIN_FSYNC3 = 6;  ///< SPI Load pin number (FSYNC in AD9833 usage)
+// const uint8_t PIN_FSYNC4 = 7;  ///< SPI Load pin number (FSYNC in AD9833 usage)
 
 MD_AD9833	AD1(PIN_DATA, PIN_CLK, PIN_FSYNC1); // Arbitrary SPI pins
 MD_AD9833	AD2(PIN_DATA, PIN_CLK, PIN_FSYNC2); // Arbitrary SPI pins
 MD_AD9833	AD3(PIN_DATA, PIN_CLK, PIN_FSYNC3); // Arbitrary SPI pins
-MD_AD9833	AD4(PIN_DATA, PIN_CLK, PIN_FSYNC4); // Arbitrary SPI pins
+// MD_AD9833	AD4(PIN_DATA, PIN_CLK, PIN_FSYNC4); // Arbitrary SPI pins
 
 #define SILENT_FREQ 1000000
 
@@ -61,13 +61,13 @@ void setup(void)
   AD2.setFrequency(0, SILENT_FREQ);
   AD3.begin();
   AD3.setFrequency(0, SILENT_FREQ);
-  AD4.begin();
-  AD4.setFrequency(0, SILENT_FREQ);
+  // AD4.begin();
+  // AD4.setFrequency(0, SILENT_FREQ);
 
   AD1.setMode(MD_AD9833::MODE_SINE);
   AD2.setMode(MD_AD9833::MODE_SINE);
   AD3.setMode(MD_AD9833::MODE_SINE);
-  AD4.setMode(MD_AD9833::MODE_SINE);
+  // AD4.setMode(MD_AD9833::MODE_SINE);
 
   rows[0] = ROW1;
   rows[1] = ROW2;
@@ -203,29 +203,29 @@ void error_tone(){
   AD1.setFrequency(0, SILENT_FREQ);
 }
 
-void offhook_tone(int times=20){
-  for(int i = 0; i < times; i++){
-    AD1.setFrequency(0, 1400.0);
-    AD2.setFrequency(0, 2060.0);
-    AD3.setFrequency(0, 2450.0);
-    AD4.setFrequency(0, 2600.0);
-    delay(100);
+// void offhook_tone(int times=20){
+//   for(int i = 0; i < times; i++){
+//     AD1.setFrequency(0, 1400.0);
+//     AD2.setFrequency(0, 2060.0);
+//     AD3.setFrequency(0, 2450.0);
+//     AD4.setFrequency(0, 2600.0);
+//     delay(100);
 
-    AD1.setFrequency(0, SILENT_FREQ);
-    AD2.setFrequency(0, SILENT_FREQ);
-    AD3.setFrequency(0, SILENT_FREQ);
-    AD4.setFrequency(0, SILENT_FREQ);
+//     AD1.setFrequency(0, SILENT_FREQ);
+//     AD2.setFrequency(0, SILENT_FREQ);
+//     AD3.setFrequency(0, SILENT_FREQ);
+//     AD4.setFrequency(0, SILENT_FREQ);
 
-    delay(100);
-  }
-}
+//     delay(100);
+//   }
+// }
 
 void loop(void){
   dial_tone();
   dial_random_phone();
   delay(500);
  
-  int outcome = random(0, 4);
+  int outcome = random(0, 3);
   switch(outcome){
     case 0:
       busy_tone();      
@@ -236,11 +236,11 @@ void loop(void){
     case 2:
       reorder_tone();
       break;
-    case 3:
-      error_tone();
-      delay(4000);
-      offhook_tone();
-      break;
+    // case 3:
+    //   error_tone();
+    //   delay(4000);
+    //   offhook_tone();
+    //   break;
   }
 
   delay(2000);
